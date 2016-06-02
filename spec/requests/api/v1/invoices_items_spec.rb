34d039d034d1f12dcff5633606828d3c3254f468
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-06-01 22:59:10 -0600

    add invoice_items_spec

---
+++ spec/requests/api/v1/invoices_items_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe "GET /api/v1/invoices/:id/items" do
+  it "returns the items belonging to the invoice" do
+    invoice       = create(:invoice)
+    item1         = create(:item)
+    invoice_item  = create(:invoice_item, invoice: invoice, item: item1)
+    item2         = create(:item, name: "hellooo")
+    invoice_item  = create(:invoice_item, invoice: invoice, item: item2)
+
+    get "/api/v1/invoices/#{invoice.id}/items"
+
+    expect(parsed_json.count).to eq(2)
+
+    parsed_item = parsed_json.first
+    expect(parsed_item).to eq(
+    "id"             =>  item1.id,
+    "name"           =>  item1.name,
+    "description"    =>  item1.description,
+    "unit_price"     =>  item1.unit_price,
+    "merchant_id"    =>  item1.merchant.id
+    )
+  end
+end
