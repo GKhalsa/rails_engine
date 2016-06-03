7e0f9cdf652ba2468784647102513d65cda5f6f8
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-06-03 00:19:40 -0600

    add items ranked by revenue spec

---
+++ spec/requests/api/v1/items_ranked_by_revenue_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe "GET /api/v1/items/most_revenue?quantity=x" do
+  it "returns highest grossing items by revenue" do
+        merchant   =  create(:merchant)
+            item   =  create(:item, name: "what", merchant: merchant)
+           item2   =  create(:item)
+         invoice   =  create(:invoice, merchant: merchant)
+    invoice_item   =  create(:invoice_item, item: item, invoice: invoice)
+
+    get "/api/v1/items/most_revenue?quantity=#{1}"
+
+    expect(parsed_json.count).to eq(1)
+    expect(parsed_json.first).to eq(
+    "id"          =>  item.id,
+    "name"        =>  item.name,
+    "description" => item.description,
+    "unit_price"  => "0.01",
+    "merchant_id" => merchant.id
+    )
+  end
+end
